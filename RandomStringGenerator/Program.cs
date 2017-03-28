57d6715a2141a992964fc85ac0273ac033fb378b
stevieray8450 <stevieray8450@gmail.com>
2017-03-27 21:22:10 -0500

    first commit 03/27 @ 21:22

--- RandomStringGenerator/Program.cs
+++ RandomStringGenerator/Program.cs
@@ -10,6 +10,31 @@ namespace RandomStringGenerator
     {
         static void Main(string[] args)
         {
+            string randoString = "";
+            Random randIndex = new Random();
+            int randIndexNum;
+
+            char[] charBank = new char[12];
+            charBank[0] = '_';
+            charBank[1] = 'b';
+            charBank[2] = '?';
+            charBank[3] = 'Z';
+            charBank[4] = 'x';
+            charBank[5] = 'B';
+            charBank[6] = '3';
+            charBank[7] = 'D';
+            charBank[8] = '1';
+            charBank[9] = '2';
+            charBank[10] = '3';
+            charBank[11] = '!';
+
+            for (int i = 0; i < charBank.Length; i++)
+            {
+                randIndexNum = randIndex.Next(0, 11);
+                randoString += charBank[randIndexNum];
+            }
+
+            Console.WriteLine(randoString);
         }
     }
 }
